ef07adeeaea7a3c633c46210fc9f8a960c70276f
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-11-21 16:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const myForm=document.getElementById("myForm");
+
+
+function onSubmit(e){
+	e.preventDefault()
+	if(e.target[0].value=="" || e.target[1].value==""){
+		alert("Please enter valid details")
+	}
+	const promise=new Promise((resolve,reject)=>{
+		if(e.target[0].value>18){
+			 setTimeout(()=>{
+			 	resolve("Welcome, . You can vote.")
+			 },4000)
+		}else{
+			setTimeout(()=>{
+				reject("Oh sorry . You aren't old enough.")
+			},4000)
+		}
+	})
+
+	promise.then((output)=>{
+		alert(`${output}`)
+	}).catch((e)=>{
+		alert(`${e}`)
+	})
+}
+
+myForm.addEventListener("submit",onSubmit)
